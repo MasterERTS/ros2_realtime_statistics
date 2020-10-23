3559eb757c0b0b9e5ab81d71518907677d507ac2
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-10-23 10:34:02 +0200

    readd files

---
+++ README.md
@@ -0,0 +1,169 @@
+[![Say Thanks][saythanks-shield]][saythanks-url]
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![HitCount](http://hits.dwyl.com/mastererts/ros2_realtime_statistics.svg)](http://hits.dwyl.com/mastererts/ros2_realtime_statistics)
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+<p align="center">
+    <!--- relative path means image/image.png instead of https://etc... -->
+    <img src="data/not_stressed/rt_nrt_Driver_comparison.png" width="600" height="415">                           
+</a>
+
+  <h3 align="center">ROS2 JSON</h3>
+
+  <p align="center">
+    Subscribe to ROS2 realtime statistics topics and manipulate the data.
+    <br />
+    <a href="https://github.com/MasterERTS/ros2_json/blob/master/README.md"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/MasterERTS/ros2_json">View Demo</a>
+    ·
+    <a href="https://github.com/MasterERTS/ros2_json/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/MasterERTS/ros2_json/issues">Request Feature</a>
+  </p>
+</p>
+
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Setup](#setup)
+* [Goal](#goal)
+* [Roadmap](#roadmap)
+* [Contribute](#contribute)
+* [License](#license)
+* [Contact](#contact)
+* [Contributors](#contributors)
+
+## About the Project
+
+This project aims at delivering a package which is capable of subscribing to a ROS2 topic and store the messages passed through the topic in JSON format. The resultant JSON file can be used for better visualization of data, using plots or charts. This is particularly useful in the logging and analysis of real-time data, especially monitoring the context switches, and use of shared resouces.
+
+# Installation
+
+## RT Pendulum Demo
+
+First, get and set the pendulum package here : https://github.com/ros2-realtime-demo/pendulum
+
+```sh
+$ source /opt/ros/eloquent/setup.bash
+$ mkdir -p ~/pendulum_ws/src
+$ cd ~/pendulum_ws/src
+$ git clone https://github.com/ros2-realtime-demo/pendulum
+$ cd ~/pendulum_ws
+$ sudo rosdep init
+$ rosdep update
+$ rosdep install -q -y --from-paths src --ignore-src --rosdistro eloquent
+$ colcon build --merge-install # OR colcon build --symlink-install
+```
+
+Then try and run some of these : https://github.com/ros2-realtime-demo/pendulum/blob/eloquent/docs/real_time_tutorial.md
+
+## RealTime Statistics
+
+```sh
+$ mkdir -p ~/rt_stats_ws
+$ git clone https://github.com/MasterERTS/ros2_realtime_statistics.git
+$ colcon build --symlink-install
+$ . install/setup.bash # or setup.zsh if on zsh
+```
+
+# Run 
+Terminal 1 :
+```sh
+$ cd ~/pendulum_ws
+$ . install/setup.bash
+$ ros2 run pendulum_demo pendulum_demo --priority 80 --pub-stats
+```
+Terminal 2 :
+```sh
+$ cd ~/pendulum_ws
+$ install/setup.bash
+$ ros2 run pendulum_manager pendulum_manager
+press 0 to activate all nodes
+```
+Terminal 3 :
+```sh
+$ cd ~/rt_stats_ws
+$ . install/setup.bash
+$ ros2 run realtime_statistics write_json --time 30
+--time is a required argument, its value is an int (time simulated in seconds)
+```
+Terminal 4 : 
+```sh
+$ cd ~/rt_stats_ws
+$ . install/setup.bash
+$ ros2 run realtime_statistics plot_json --mode "both" --plot "both"
+--mode takes "driver", "controller" and "both" as accepted values. It plots the corresponding data set.
+--plot takes "rt", "nrt" and "both" as accepted values. It plots the RT dataset, the NRT dataset or compares both.
+```
+
+## Roadmap
+
+See the [open issues](https://github.com/guilyx/gantt-trampoline/issues) for a list of proposed features (and known issues).
+
+## Contribute
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**. (please help)
+
+### Contribute on proposed features
+
+1. Choose any open issue from [here](https://github.com/guilyx/gantt-trampoline/issues).
+2. Comment on the issue: `Can I work on this?` and get assigned.
+3. Make changes to your `Fork` and send a PR.
+
+Otherwise just create the issue yourself, and we'll discuss and assign you to it if serves the project !
+
+To create a PR:
+
+Follow the given link to make a successful and valid PR: https://help.github.com/articles/creating-a-pull-request/
+
+To send a PR, follow these rules carefully, **otherwise your PR will be closed**:
+
+1. Make PR title in this formats:
+```
+Fixes #IssueNo : Name of Issue
+```
+```
+Feature #IssueNo : Name of Issue
+```
+```
+Enhancement #IssueNo : Name of Issue
+```
+
+According to what type of issue you believe it is.
+
+For any doubts related to the issues, i.e., to understand the issue better etc, comment down your queries on the respective issue.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+
+Erwin Lejeune - [@spida_rwin](https://twitter.com/spida_rwin) - erwin.lejeune15@gmail.com
+Sampreet Sarkar - sampreet.sarkar@eleves.ec-nantes.fr
+
+## Contributors
+
+- [Erwin Lejeune](https://github.com/Guilyx)
+- [Sampreet Sarkar](https://github.com/sampreets3)
+
+[saythanks-shield]:https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg?style=flat_square
+[saythanks-url]:https://saythanks.io/to/erwin.lejeune15%40gmail.com
+[contributors-shield]: https://img.shields.io/github/contributors/mastererts/ros2_realtime_statistics.svg?style=flat-square
+[contributors-url]: https://github.com/mastererts/ros2_realtime_statistics/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/mastererts/ros2_realtime_statistics.svg?style=flat-square
+[forks-url]: https://github.com/mastererts/ros2_realtime_statistics/network/members
+[stars-shield]: https://img.shields.io/github/stars/mastererts/ros2_realtime_statistics.svg?style=flat-square
+[stars-url]: https://github.com/mastererts/ros2_realtime_statistics/stargazers
+[issues-shield]: https://img.shields.io/github/issues/mastererts/ros2_realtime_statistics.svg?style=flat-square
+[issues-url]: https://github.com/mastererts/ros2_realtime_statistics/issues
+[license-shield]: https://img.shields.io/github/license/mastererts/ros2_realtime_statistics.svg?style=flat-square
+[license-url]: https://github.com/mastererts/ros2_realtime_statistics/blob/master/LICENSE.md
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/erwinlejeune-lkn
---
+++ src/realtime_statistics/realtime_statistics/write_json.py
@@ -0,0 +1,205 @@
+# Copyright 2020 Erwin Lejeune
+# Under MIT Licence
+# <erwin.lejeune15@gmail.com>
+
+import rclpy
+from rclpy.node import Node
+import json
+from time import time
+import argparse
+import subprocess
+import pathlib
+from pendulum_msgs_v2.msg import ControllerStats
+from pendulum_msgs_v2.msg import PendulumStats
+
+cmd1 = subprocess.Popen(["dmesg"], stdout=subprocess.PIPE)
+cmd2 = subprocess.Popen(
+    ["grep", "-i", "xenomai"], stdin=cmd1.stdout, stdout=subprocess.PIPE
+)
+output, err = cmd2.communicate()
+
+if "Xenomai" in str(output):
+    RT = 1
+else:
+    RT = 0
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Real-Time Statistics Plotter.")
+    parser.add_argument(
+        "--time",
+        type=int,
+        required=True,
+        help="Input simulated time in seconds for data fetching.",
+        default=30,
+    )
+    return parser.parse_args()
+
+
+class WriteJson(Node):
+    def __init__(self):
+        super().__init__("node")
+
+        self.last_controller_msg = ""
+        self.last_driver_msg = ""
+
+        self.sub_controller_stats = self.create_subscription(
+            ControllerStats,
+            "/controller_statistics",
+            self.controller_statistics_callback,
+            10,
+        )
+        self.sub_pendulum_stats = self.create_subscription(
+            PendulumStats, "/driver_statistics", self.driver_statistics_callback, 10
+        )
+
+        default_path = str(pathlib.Path(__file__).parent.absolute())
+        default_path = default_path.replace(
+            "/build/realtime_statistics/realtime_statistics", ""
+        )
+
+        if RT == 0:
+            self.driver_stats_path = (
+                default_path + "/ros2_realtime_statistics/data/driver_nrt_stats.json"
+            )
+            self.controller_stats_path = (
+                default_path
+                + "/ros2_realtime_statistics/data//controller_nrt_stats.json"
+            )
+        else:
+            self.driver_stats_path = (
+                default_path + "/ros2_realtime_statistics/data/driver_rt_stats.json"
+            )
+            self.controller_stats_path = (
+                default_path
+                + "/ros2_realtime_statistics/data//controller_rt_stats.json"
+            )
+
+        self.create_json_files()
+
+        self.data_number_controller = 0
+        self.data_number_driver = 0
+
+        self.sub_controller_stats  # prevent unused variable
+        self.sub_pendulum_stats  # prevent unused variable warning
+
+    def controller_statistics_callback(self, msg):
+        if str(msg) != self.last_controller_msg:
+            data = self.convert_msg_to_dict(msg, self.data_number_controller)
+            self.add_data_to_json(data, self.controller_stats_path)
+            self.data_number_controller += 1
+            self.last_controller_msg = str(msg)
+
+    def driver_statistics_callback(self, msg):
+        if str(msg) != self.last_driver_msg:
+            data = self.convert_msg_to_dict(msg, self.data_number_driver)
+            self.add_data_to_json(data, self.driver_stats_path)
+            self.data_number_driver += 1
+            self.last_driver_msg = str(msg)
+
+    def create_json_files(self):
+        empty_data = {}
+
+        with open(self.driver_stats_path, "w") as dfile:
+            json.dump(empty_data, dfile)
+        with open(self.controller_stats_path, "w") as cfile:
+            json.dump(empty_data, cfile)
+
+    def convert_msg_to_dict(self, msg, rank):
+        lst_msg = str(msg).split("pendulum_msgs_v2.msg.")
+        msg_dict = {}
+        msg_dict[rank] = {}
+        fields_list = []
+        key_value_list = []
+        lst_msg.pop(0)
+        first_round = True
+
+        ## Dump non relevant data
+        for elem in lst_msg:
+            if first_round:
+                fields_list.append([lst_msg[0]])
+                if ("ControllerStats(") in fields_list[0][0]:
+                    fields_list[0][0] = fields_list[0][0].replace(
+                        "ControllerStats(", ""
+                    )
+                elif ("PendulumStats(") in fields_list[0][0]:
+                    fields_list[0][0] = fields_list[0][0].replace("PendulumStats(", "")
+                first_round = False
+
+            if ("TimerStats") in elem:
+                elem = elem.replace("TimerStats", "")
+            elif ("ControllerStats") in elem:
+                elem = elem.replace("ControllerStats", "")
+            elif ("PendulumStats") in elem:
+                elem = elem.replace("PendulumStats", "")
+            elif ("TopicStats") in elem:
+                elem = elem.replace("TopicStats", "")
+            elif ("Rusage") in elem:
+                elem = elem.replace("Rusage", "")
+
+            if "(" in elem:
+                elem = elem.replace("(", "")
+            if ")" in elem:
+                elem = elem.replace(")", "")
+            if "" == elem:
+                elem.remove(elem)
+            if " " in elem:
+                elem = elem.replace(" ", "")
+
+            if (",") in elem:
+                fields_list.append(elem.split(","))
+
+        for elem in fields_list:
+            for x in elem:
+                if "=" in x:
+                    key_value_list.append(x.split("="))
+
+        for x in range(len(fields_list)):
+            for y in range(len(fields_list[x])):
+                if "stats" in fields_list[x][y]:
+                    fields_list[x][y] = fields_list[x][y].replace(" ", "")
+                    fields_list[x][y] = fields_list[x][y].replace("=", "")
+                    msg_dict[rank][fields_list[x][y]] = {}
+                    for elem in key_value_list:
+                        for l in elem:
+                            if "stats" in l or l == "":
+                                continue
+                            else:
+                                msg_dict[rank][fields_list[x][y]][elem[0]] = float(
+                                    elem[1]
+                                )
+
+        return msg_dict
+
+    def add_data_to_json(self, data, path):
+        with open(path, "r+") as file:
+            try:
+                json_data = json.load(file)
+                json_data.update(data)
+                file.seek(0)
+                json.dump(json_data, file, indent=4)
+            except json.decoder.JSONDecodeError:
+                pass
+
+
+def main(args=None):
+    argprs = get_args()
+    rclpy.init(args=args)
+
+    writejson = WriteJson()
+
+    while (
+        writejson.data_number_controller/10 <= argprs.time
+        or writejson.data_number_driver/10 <= argprs.time
+    ):
+        rclpy.spin_once(writejson)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    writejson.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
